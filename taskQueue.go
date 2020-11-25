19220d737f1ac8426d9585f1976140948655c839
Indrajeet Khandekar <indrajeet.khandekar@taranawireless.com>
2020-11-25 23:17:01 +0530

    Add enque and dequeue methods for taskQueue

--- taskQueue.go
+++ taskQueue.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 type TaskQueue struct {
 	Tasks []Task
 }
@@ -7,6 +11,19 @@ type TaskQueue struct {
 type Task struct {
 }
 
-func (q *TaskQueue) size() (int) {
+func (q *TaskQueue) size() int {
 	return len(q.Tasks)
+}
+
+func (q *TaskQueue) dequeue() (t *Task, err error) {
+	var task Task
+	if q.size() > 0 {
+		task, q.Tasks = q.Tasks[0], q.Tasks[1:]
+		return &task, nil
+	}
+	return nil, fmt.Errorf("Cannot dequeue from an empty queue")
+}
+
+func (q *TaskQueue) enqueue(t *Task) {
+	q.Tasks = append(q.Tasks, *t)
 }
--- taskQueue_test.go
+++ taskQueue_test.go
@@ -6,9 +6,49 @@ import (
 )
 
 func TestQueue(t *testing.T) {
-	t.Run("Test can create a zero length task queue", func(t *testing.T){
-		q := TaskQueue {}
+	t.Run("can create a zero length task queue", func(t *testing.T) {
+		q := TaskQueue{}
 		fmt.Printf("Queue size is %d\n", q.size())
 	})
-}
 
+	t.Run("can add a task to task queue", func(t *testing.T) {
+		q := TaskQueue{}
+		task := Task{}
+
+		q.enqueue(&task)
+
+		if q.size() != 1 {
+			t.Error("Queue length should increase after task is added")
+		}
+
+	})
+
+	t.Run("can remove a task from task queue", func(t *testing.T) {
+		q := TaskQueue{}
+		task := Task{}
+
+		q.enqueue(&task)
+
+		if q.size() != 1 {
+			t.Error("Queue length should increase after task is added")
+		}
+
+		_, err := q.dequeue()
+
+		if err != nil {
+			t.Error("There should be no error when deleting from a populated queue")
+		}
+		if q.size() != 0 {
+			t.Error("Queue length should decrease after task is removed")
+		}
+	})
+
+	t.Run("removing a from empty task queue returns an error", func(t *testing.T) {
+		q := TaskQueue{}
+		_, err := q.dequeue()
+
+		if err == nil {
+			t.Error("dequeue from an empty queue should issue error")
+		}
+	})
+}
